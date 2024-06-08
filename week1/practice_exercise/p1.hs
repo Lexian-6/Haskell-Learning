93d4ac0ee8b8d3b0c6625202fa45dd5cd6afcbbb
Junle <z5431301@ad.unsw.edu.au>
2024-06-08 16:10:54 +1000

    Week 1 Practice problems

---
+++ week1/practice_exercise/p1.hs
@@ -0,0 +1,25 @@
+module Practice1 where
+
+{-
+mySum :: [Int] -> Int
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+myProduct :: [Int] -> Int
+myProduct [] = 1
+myProduct (x:xs) = x * myProduct xs
+-}
+a::[Int]
+a=[1,2,3]
+
+myBinop :: (Int -> Int -> Int) -> Int -> ([Int] -> Int)
+myBinop f z [] = z
+myBinop f z (x:xs) = f x (myBinop f z xs)
+
+mySum :: [Int] -> Int
+myProduct :: [Int] -> Int
+mySum = foldr (+) 0
+myProduct = foldr (*) 1
+
+myMap :: ( a -> b ) -> [a] -> [b]
+myMap f = foldr (\x -> (f x :)) []
